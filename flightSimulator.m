9a7b5e232bfe1bf8aa62aa5b57dc0f8bd241de63
Olivinitic <osalmeron32@gmail.com>
2016-04-17 03:50:07 -0700

    Flight Simulator

--- flightSimulator.m
+++ flightSimulator.m
@@ -1,42 +1,38 @@
 %------------------------------------------------------------------------
 % Program name  : Flight Path Simulation/Motion Tracking
-% 
+% Team          : Phoenix College
+% College       : Ursa Major Ascending Forward
+% Year          : 2015-16
 % Author        : Oliver Salmeron
 %
-% Version       : 1.2
+% Goal          : Collaborate post processing algorithms amongst teams to share,
+% improve and optimtize code. Read the License.txt for more info on the
+% idea.
 %
-% Description   : Matlab script for processing IMU [and GPS] data to simulate the flight motion and path. 
+% Description   : Matlab script for processing IMU data to simulate the flight motion and path. 
 % 
-% This program will open a text file containing flight data (roll,pitch,yaw,lat,lon,alt) to rotate a 3D
-% object. The result is saved as a movie named 'flightSimulation.mp4'.
+% This program will open a text file containing flight data (roll,pitch,yaw) to rotate a 3D
+% object. The result is saved as a movie named 'flightSimulation.mp4' within the same folder.
 % This will help to visualize the motion tracking of a payload during the course of a flight.
-%
-% To look up a MATLAB command, highlight command and press F1.
+% 
+% To look up a MATLAB command, highlight command and press F1. Or, type:
+% help "command" (without exclamations).
 %
 % Steps Outline :
 %   1. Generate Grid.
-%   2. Generate Cylinder and Frame vectors.
-%   3. Plot the Cylinder and Frame vectors.
-%   4. Create a Group Object and Parent Surface.
-%   5. Import 'flightData.txt' text file for reading.
+%   2. Generate Cylinder and Frame vectors -- Used to create object.
+%   3. Plot the Cylinder and Frame vectors -- Plot shapes.
+%   4. Create a Group Object and Parent Surface -- Make object using shapes
+%   5. Import 'flightData.txt' text file for reading -- include full file
+%   path.
 %   6. Open/Close text file to read and exctract data.
 %   7. Parse imported data into temporary variables.
-%   8. Create Video File to write to.
-%   9. Create a set of frames and write each frame to Video File.
+%   8. Create Video File to write to -- "flightSimulation".
+%   9. For Loop -- Plot the frames and save to Video file.
+%   10. Close Video File.
 %
-%   Useful MATLAB Commands:
-%       colormap
-%       
+% To do's       : Add here
 %
-%
-%
-% To do : Update script to integrate GPS data
-%         Integrate MATLAB with Arduino for faster and easier testing
-%           -Add ArduinoIO package to MATLAB
-%         Update script to a function
-%         Update to read a csv file (instead of import text file)?
-%
-%         Color maps: colormap(gray), colormap hsv, colormap copper
 %-------------------------------------------------------------------------
 
 %% 1. Generate Grid
@@ -57,63 +53,60 @@ zlabel('z')
 
 %% 2. Generate the Cylinder and frame vectors.
 
-% draw the three coloured frame lines
+% draw the three coloured frame vectors
 Lx = line([0 1.25],[0,0],[0,0],'color',[1,0,0]);
 Ly = line([0 0],[0,1.25],[0,0],'color',[0,1,0]);
 Lz = line([0 0],[0,0],[0,1.25],'color',[0,0,1]);
 	
 set([Lx,Ly,Lz],'linewidth',3)
     
-% add text for x,y and z for each frame line
+% add text for x,y and z for each frame vector
 tX = text('position',[.7 0 .1],'string','x','fontw','b');
 tY = text('position',[ 0 .7 .1],'string','y','fontw','b');
 tZ = text('position',[.05 0.05 .7],'string','z','fontw','b');
 	
-% draw the three base frame axis lines for reference
-line([0 1.5],[0,0],[0,0],'color',[0,0,0],'linewidth',1);
-line([0 0],[0,1.5],[0,0],'color',[0,0,0],'linewidth',1);
-line([0 0],[0,0],[0,1.5],'color',[0,0,0],'linewidth',1);
+% draw the three axis vectors for reference (note the origin offset)
+line([0,1.5],[0,0],[0,0],'color',[0,0,0],'linewidth',1);
+line([0,0],[0,1.5],[0,0],'color',[0,0,0],'linewidth',1);
+line([0,0],[0,0],[0,1.5],'color',[0,0,0],'linewidth',1);
 	
 % add text for x0,y0 and z0 for each base frame line
 text('position',[1.3 0 .1],'string','x_0','fontw','b');
 text('position',[ 0 1.3 .1],'string','y_0','fontw','b');
 text('position',[.05 0.05 1.3],'string','z_0','fontw','b');
 
-% add text to display altitude
-
 % create Cylinder
-[X,Y,Z] = cylinder([0.75 0.75]);	
+[X,Y,Z] = cylinder([0.75 0.75]);
 
 % create Sphere
 [S1,S2,S3] = sphere;
-
     
 %% 3. Plot the shapes/Create the object
-% Plot the cylinder (Body)
-	
+
+% Plot the cylinder (Body)	
 h(1) = surf(X.*0.5,Y.*0.5,Z.*0.5);
 h(2) = surf(-X.*0.5,-Y.*0.5,-Z.*0.5);
 
 % Plot the Sphere (Bottom)
-h(9)  = surf(S1.*0.375,S2.*0.375,S3.*0.375-0.5);
+h(3)  = surf(S1.*0.375,S2.*0.375,S3.*0.375-0.5);
 
-% Plot the Circle (Lid)
-h(10) = surf(S1.*0.375,S2.*0.375,S3.*0+0.5);
-h(11) = mesh(S1.*0.1,S2.*0.1,S3.*0.05+0.5);
+% Plot the Circle (Lid) using sphere
+h(4) = surf(S1.*0.375,S2.*0.375,S3.*0+0.5);
+h(5) = mesh(S1.*0.1,S2.*0.1,S3.*0.05+0.5);
 
-%Set the colormap (lots to look up)
+%Set the colormap
 colormap(gray)
 
 % Plot the Base and Frame Vectors
-h(3) = Lx;
-h(4) = Ly;
-h(5) = Lz;
+h(6) = Lx;
+h(7) = Ly;
+h(8) = Lz;
 
-h(6) = tX;
-h(7) = tY;
-h(8) = tZ;
+h(9) = tX;
+h(10) = tY;
+h(11) = tZ;
 
-% plot origin point
+% plot origin point at the center of the object
 plot3(0,0,0,'x','markersize',20,'color','k')
 
 %% 4. Create a group object and parent surface 
@@ -125,17 +118,18 @@ drawnow
 
 %% 5. Import 'flightData' text file for reading. 
 
-% Use the Import tool from the home tab to visually select data to extract from a
-% file and generate script.
+% Used the Import tool from the home tab to visually select data to extract from a
+% file and then generate the following script. The data file can be named
+% anything but it needs to be consistent with the filename.
 
 %Initialize variables.
 
 % Only the filename needs to be updated with the correct full file path.
-filename = 'C:\Users\Oliver\Desktop\MATLAB Functions\Flight Path Processing\flightData.txt';
+filename = 'C:\Users\Oliver\Documents\GitHub\flightPathSimulator\flightData.txt';
 delimiter = '\t';
 startRow = 2;
     
-% Format string for each line of text:
+% Format string for each line of text (formatSpec):
     %   column1: double (%f)
     %	column2: double (%f)
     %   column3: double (%f)
@@ -159,13 +153,6 @@ dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,N
     
 % Close the text file.
 fclose(fileID);
-   
-    % To include COM port for serial interface, use the command line below.
-    % This would help for live testing of code. If used, comment out the
-    % File Import section.
-    
-    %a= arduino('com3');        % Select COM port
-
 
 %%  7. Parse imported data into temporary variables.
 
@@ -183,51 +170,44 @@ clearvars filename delimiter startRow formatSpec fileID dataArray ans;
     
 %% 8. Create Video File to write to.
 v = VideoWriter('flightSimulation.mp4','MPEG-4')         % Create an mp4 file type and display properties
+v.FrameRate = 30;          % Change the Frame Rate of the exported video
 open(v);
 %axis tight manual
 set(gca,'nextplot','replacechildren');
 
 %% 9. Create a set of frames and write each frame to Video File.
+% Define the conversion factors
     radians_degree = pi/180;    % Conversion factor from Degrees to Radians.
-    feet_meter = 3.28084;        % Conversion factor from Meters to Feet (1m=3.28ft)
-    
-for Time = 7550:7708     % Update limit of while loop to Max Time
-    % Read GPS values (lat,lon,alt-->lat,lon,a).
-    %lat = lat(Time);
-    %lon = lon(Time);
+    feet_meter = 3.28084;       % Conversion factor from Meters to Feet (1m=3.28ft)
+    R = 6371;                   % R = Radius of earth
 
+%% 9. For Loop
+for Time = 1:7000     % Update limits of while loop to the desired Time frame
 
-    
-    
     % Read Euler angles (roll,pitch,yaw-->r,p,y).
     r = roll(Time);         % r <-- roll reading
     p = pitch(Time);        % p <-- pitch reading
     y = yaw(Time);          % y <-- yaw reading
-     
+        
     % Convert Euler angles to (X,Y,Z) axes rotations in radians.
     X = r*radians_degree;   % X <-- r
     Y = p*radians_degree;   % Y <-- p
     Z = y*radians_degree;   % Z <-- y
-   
-    % Rotate object in steps.
+    
+    % Create a Rotatation Matrix using axes rotations
     Rot = makehgtform('xrotate',X,'yrotate',Y,'zrotate',Z);
     set(combinedobject,'Matrix',Rot)
-    
-    % Convert alt to feet and plot as title
-    %a = alt(Time);
-    %alt = a*feet_meter;
-    %plot3(0,0,1,text(0,0,1,['alt: ',int2str(alt)]));
-    
+  
+    % New Plot
     drawnow;
-    %pause(0.0001);        % Set the plot rate
+    pause(0.0001);            % Change the plot rate of MATLAB animation (not video)
     
-
-    
-    axis([-1 1 -1 1 -1 1])  % Set the axis for each frame of the loop for recording a movie
-    frame = getframe;       
-    writeVideo(v,frame);    % Record Movie of the frames   
+    axis([-1.5 1.5 -1.5 1.5 -1.5 1.5])  % Set the axis for each frame of the loop for recording a movie
+    frame = getframe;       % Get frame
+    writeVideo(v,frame);    % Save frame to Video. Rename afterwards for description.
    
     Time=Time+1;            % Increment the for loop
 end
 
+%% 10. Close video
 close(v);
